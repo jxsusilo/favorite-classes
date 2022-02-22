820d30d3d999735cb01ba25a95d4f88f2425fd97
Jessica Susilo <jxsusilo@gmail.com>
2022-02-21 22:30:52 -0800

    Added class descriptions

---
+++ src/components/Class/Class.js
@@ -0,0 +1,53 @@
+import "./Class.css";
+import React, {useState, useEffect} from 'react';
+
+function Class(props) {
+
+    const [classInfo, setClassInfo] = useState({});
+
+    const url = 'https://api.peterportal.org/rest/v0/courses/'
+
+    useEffect (() => {
+        const fetchData = async () => {
+            const response = await fetch(url + props.name);
+            const data = await response.json();
+            console.log(data);
+            setClassInfo(data);
+        }
+        fetchData();
+    }, [props.name])
+
+    let info;
+    if(classInfo.id) {
+        info = <div className="information">
+            <p className="title">
+                {classInfo.title}
+            </p>
+            <p className="department">
+                {classInfo.department_name}
+            </p>
+            <p className="description">
+                {classInfo.description}
+            </p>
+        </div>
+    }
+    else if(classInfo.error) {
+        info = <p>Class not found.</p>
+    }
+    else {
+        info = <p>Loading...</p>
+    }
+
+    
+
+    return (
+        <div className="class">
+            {props.name}
+            <div>
+                {info}
+            </div>
+        </div>
+    )
+}
+
+export default Class;
