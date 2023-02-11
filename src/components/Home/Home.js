f4d7a70ab072232aef046e4288bc92e7e6274739
Jessica Susilo <jxsusilo@gmail.com>
2023-02-10 16:50:22 -0800

    Removed duplicate of Class in Home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,5 @@
 import React, {useState} from "react";
 import Class from "../Class/Class";
-import ClassGraphQL from "../Class/ClassGraphQL";
 import "./Home.css"
 
 function Home(props) {
@@ -30,11 +29,6 @@ function Home(props) {
             <div id="classes">
                 {favoriteClasses.map((favClass) =>
                     <Class name={favClass} key={favClass}></Class>
-                )}
-            </div>
-            <div id="classes">
-                {favoriteClasses.map((favClass) =>
-                    <ClassGraphQL name={favClass} key={favClass}></ClassGraphQL>
                 )}
             </div>
         </div>
